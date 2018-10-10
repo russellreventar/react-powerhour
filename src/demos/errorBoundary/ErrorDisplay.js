2855bb37d4360d2a364107919b60e312d54f2a5e
Russell Reventar <rreventar@konradgroup.com>
2018-10-10 15:15:56 -0400

    ed

--- src/demos/errorBoundary/ErrorDisplay.js
+++ src/demos/errorBoundary/ErrorDisplay.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ErrorDisplay = ({error, errorStack}) => (
+const ErrorDisplay = ({error, errorInfo}) => (
   <div>
     <h5>Ergh, I crashed</h5>
     <details>
-      {error}
+      {error && error.toString()}
       <br />
-      {errorStack}
+      {errorInfo && errorInfo.componentStack}
     </details>
   </div>
 )
